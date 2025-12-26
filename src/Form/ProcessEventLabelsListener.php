ed7a5ebb1e8863cc762475e69d3e432f6aae9059
Kai <kai@korla.cloud>
2025-12-26 19:13:43 +0100

    fix: update processFormData listener signature for submitted fields
    Signed-off-by: Kai <kai@korla.cloud>

--- src/Form/ProcessEventLabelsListener.php
+++ src/Form/ProcessEventLabelsListener.php
@@ -2,16 +2,19 @@
 
 namespace KaiKorla\ContaoEventFormOptions\Form;
 
-use Contao\Form;
 use KaiKorla\ContaoEventFormOptions\EventOptions;
 
 class ProcessEventLabelsListener
 {
-    public function onProcessFormData(array &$submittedData, array $formData, Form $form): void
-    {
+    public function onProcessFormData(
+        array &$submittedData,
+        array $formData,
+        array $submittedFields
+    ): void {
         if (!isset($submittedData['event_id'])) {
             return;
         }
+
         $eventId = (string) $submittedData['event_id'];
 
         $options = EventOptions::getEventOptions((object) ['id' => null]);
